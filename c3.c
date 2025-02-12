d463dff2ab750de704023e7a8e063a0b95056ae3
Karthi <karthikkb15@gmail.com>
2025-02-12 21:52:57 +0530

    Update

--- c3.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main()
-{
-    int a;
-    scanf("%d", &a);
-    (a>=6 && a<10)? printf("Very Basic"):(a>4 && a<6)?printf("acidic"):(a==4)?printf("Neutral"):(a>=2 && a<4)?printf("Very Acidic"):printf("Acidic");
-    //ternary opp
-}
--- testc.c
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-int main(){
-int a=10;
-printf("a = %d\n",a);
-}
